4e0c03a621e7ced90be2e218f6c5e1a08a7b4e68
Kha Nguyen <nguyencaokha131995@gmail.com>
2023-09-12 12:50:26 +0200

    fix shellcheck warning

--- export-env.sh
+++ export-env.sh
@@ -6,9 +6,9 @@
 
 unamestr=$(uname)
 if [ "$unamestr" = 'Linux' ]; then
-  export $(grep -v '^#' env/dev/env.django.dev | xargs -d '\n')
-  export $(grep -v '^#' env/dev/env.postgres.dev | xargs -d '\n')
+  export "$(grep -v '^#' env/dev/env.django.dev | xargs -d '\n')"
+  export "$(grep -v '^#' env/dev/env.postgres.dev | xargs -d '\n')"
 elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
-  export $(grep -v '^#' env/dev/env.django.dev | xargs -0)
-  export $(grep -v '^#' env/dev/env.postgres.dev | xargs -0)
+  export "$(grep -v '^#' env/dev/env.django.dev | xargs -0)"
+  export "$(grep -v '^#' env/dev/env.postgres.dev | xargs -0)"
 fi
