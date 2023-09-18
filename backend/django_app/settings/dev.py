b939e9888f1536661703de08e099dd99da419ee0
Kha Nguyen <nguyencaokha131995@gmail.com>
2023-09-18 16:27:09 +0200

    adjust architect

---
+++ backend/django_app/settings/dev.py
@@ -0,0 +1,25 @@
+"""
+Configuration for development with Docker.
+"""
+import os
+
+from django_app.settings.base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("SECRET_KEY", "development")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["backend", "localhost", "127.0.0.1"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("SQL_DATABASE_NAME", "postgres"),
+        "USER": os.environ.get("SQL_USER", "postgres"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
+        "HOST": os.environ.get("SQL_HOST", "db"),  # set in docker-compose.yml
+        "PORT": os.environ.get("SQL_PORT", "5432"),  # default postgres port
+    }
+}
--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,9 +1,9 @@
 from dotenv import load_dotenv
-from common.utils import get_env_var
 from pathlib import Path
+import os
 BASE_DIR = Path(__file__).resolve().parent
 
 load_dotenv(BASE_DIR / ".env/dev/.env.django.dev")
 
 wsgi_app = "django_app.wsgi"
-bind = f"0.0.0.0:{get_env_var('DJANGO_PORT')}"
+bind = f"0.0.0.0:{os.environ.get('DJANGO_PORT')}"
